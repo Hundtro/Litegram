4fe810451a5b2893d1a6488046c3251057867942
Hundtro <dhund@protonmail.com>
2017-10-25 12:51:21 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <QApplication>
+#include <QWidget>
+#include <QMessageBox>
+#include <mainwindow.h>
+
+int main(int argc, char* argv[])
+{
+    QApplication app(argc, argv);
+
+    MainWindow m;
+
+    m.show();
+
+    return app.exec();
+}
