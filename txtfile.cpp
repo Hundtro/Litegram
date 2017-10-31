343123e91badeab298528898740cb9e88ae4b492
Hundtro <dhund@protonmail.com>
2017-10-31 08:36:25 +0100

    Update txtfile.cpp

--- txtfile.cpp
+++ txtfile.cpp
@@ -1,1 +1,16 @@
+#include <txtfile.h>
 
+std::string ImportText(std::string path)
+{
+    std::ifstream file;
+    std::string result;
+
+    file.open(path, std::ios_base::in);
+    while(file.peek()!=-1)
+    {
+        result += file.get();
+    }
+    file.close();
+
+    return result;
+}
