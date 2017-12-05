f8c8ecfe0be044ac5e12c1ac10819dead4c9096d
Hundtro <dhund@protonmail.com>
2017-12-05 14:42:43 +0100

    Create language.cpp

---
+++ language.cpp
@@ -0,0 +1,31 @@
+#include <language.h>
+
+lang en = {
+
+    "Program",
+    "Import from txt file...",
+    "Exit",
+    "Settings",
+    "Language",
+    "Help",
+    "About",
+    "Check",
+    "Clear",
+    "Select txt file to import",
+    "txt files (*.txt)"
+};
+
+lang pl = {
+
+    "Program",
+    "Import z pliku txt...",
+    "Zamknij",
+    "Ustalenia",
+    "Język",
+    "Pomoc",
+    "O programie",
+    "Weryfikuj",
+    "Wyczyść",
+    "Wybierz plik txt do wczytania",
+    "pliki txt (*.txt)"
+};
