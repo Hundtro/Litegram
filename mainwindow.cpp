add393c301a3f6decc03a7a43af831420e4f16d7
Hundtro <dhund@protonmail.com>
2017-12-05 14:45:29 +0100

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,29 +3,44 @@
 
 MainWindow :: MainWindow()
 {
+    interfaceLanguage = english;
     CreateMenuBar();
     CreateTextLayout();
     CreateButtonsLayout();
-
-    mainLayout = new QVBoxLayout();
-    mainLayout->setMenuBar(menuBar);
-    mainLayout->addLayout(textLayout);
-    mainLayout->addLayout(buttonsLayout);
-
-    this->setLayout(mainLayout);
+    SetMenuBar();
+    SetTextLayout();
+    SetButtonsLayout();
+    CreateSetMainLayout();
+    SetLanguage();
 }
 
-void MainWindow::CreateMenuBar()
-{
-    menuBar= new QMenuBar();
-    programMenu = new QMenu(tr("&Program"), this);
-    settingsMenu = new QMenu(tr("&Settings"), this);
-    helpMenu = new QMenu(tr("&Help"), this);
+void MainWindow::CreateMenuBar(){
 
-    importAction = new QAction(tr("&Import txt file..."));
-    exitAction = new QAction(tr("&Exit"));
-    languageAction = new QAction(tr("&Language"));
-    aboutAction = new QAction(tr("&About"));
+    menuBar= new QMenuBar();
+    programMenu = new QMenu();
+    settingsMenu = new QMenu();
+    helpMenu = new QMenu();
+
+    importAction = new QAction();
+    exitAction = new QAction();
+    languageAction = new QAction();
+    aboutAction = new QAction();
+}
+
+void MainWindow::CreateTextLayout(){
+
+    textLayout = new QGridLayout();
+    textEdit = new QTextEdit();
+}
+
+void MainWindow::CreateButtonsLayout(){
+
+    buttonsLayout = new QHBoxLayout();
+    checkButton = new QPushButton();
+    clearButton = new QPushButton();
+}
+
+void MainWindow::SetMenuBar(){
 
     programMenu->addAction(importAction);
     programMenu->addAction(exitAction);
@@ -41,22 +56,12 @@ void MainWindow::CreateMenuBar()
     //...
 }
 
-void MainWindow::CreateTextLayout()
-{
-    textLayout = new QGridLayout();
-    textEdit = new QTextEdit();
+void MainWindow::SetTextLayout(){
 
     textLayout->addWidget(textEdit);
 }
 
-void MainWindow::CreateButtonsLayout()
-{
-    buttonsLayout = new QHBoxLayout();
-    checkButton = new QPushButton();
-    clearButton = new QPushButton();
-
-    checkButton->setText("Check text");
-    clearButton->setText("Clear text");
+void MainWindow::SetButtonsLayout(){
 
     buttonsLayout->addWidget(checkButton);
     buttonsLayout->addWidget(clearButton);
@@ -64,19 +69,65 @@ void MainWindow::CreateButtonsLayout()
     connect(clearButton, SIGNAL(clicked()), this, SLOT(Clear()));
 }
 
+void MainWindow::CreateSetMainLayout(){
+
+    mainLayout = new QVBoxLayout();
+    mainLayout->setMenuBar(menuBar);
+    mainLayout->addLayout(textLayout);
+    mainLayout->addLayout(buttonsLayout);
+
+    this->setLayout(mainLayout);
+}
+
+void MainWindow::SetLanguage(){
+
+    switch(interfaceLanguage)
+    {
+        case english:
+            SetInterfaceText(en);
+            break;
+
+        case polish:
+            SetInterfaceText(pl);
+            break;
+    }
+}
+
+void MainWindow::SetInterfaceText(lang l){
+
+    programMenu->setTitle(l.menu_program);
+    settingsMenu->setTitle(l.menu_settings);
+    helpMenu->setTitle(l.menu_help);
+
+    importAction->setText(l.menu_import_txt);
+    exitAction->setText(l.menu_exit);
+    languageAction->setText(l.menu_language);
+    aboutAction->setText(l.menu_about);
+
+    checkButton->setText(l.buttom_check);
+    clearButton->setText(l.buttom_clear);
+}
+
 void MainWindow::Import()
 {
-    QString path = QFileDialog::getOpenFileName(this, tr("Select txt file to import"), "", tr("txt files (*.txt)"));
-    QString text = QString::fromStdString(ImportText(path.toUtf8().constData()));
-    this->textEdit->setText(text);
+    //
+    QString path;
+
+    switch(this->interfaceLanguage)
+    {
+        case english:
+            path = QFileDialog::getOpenFileName(this, en.title_import_txt, "", en.filter_import_txt);
+            break;
+        case polish:
+            path = QFileDialog::getOpenFileName(this, pl.title_import_txt, "", pl.filter_import_txt);
+            break;
+    }
+
+    QString text = ImportText(path);
+    textEdit->setText(text);
 }
 
 void MainWindow::Clear()
 {
-    this->textEdit->clear();
-}
-
-void MainWindow::Close()
-{
-    exit(0);
+    textEdit->clear();
 }
